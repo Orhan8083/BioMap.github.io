1030bce9c878696b0326e0f219131600d4e45071
Cro-Magnon <108482875+Orhan8083@users.noreply.github.com>
2023-10-08 15:52:57 +0300

    added files

---
+++ script.js
@@ -0,0 +1,29 @@
+function changeClimateMap() {
+    var img = document.getElementById('image');
+    img.src = 'güneyafrika.png';
+}
+
+function Reset() {
+    var img = document.getElementById('image');
+    img.src = 'GüneyAfrikaDefault.png';
+}
+
+function changePlantationMap() {
+    var img =document.getElementById('image')
+    img.src = 'güneyafrikacoğrafya.png'
+}
+
+function changePopulationMap() {
+    var img =document.getElementById('image')
+    img.src = 'Popülasyonharitası.png'
+}
+
+function changeTopography() {
+    var img =document.getElementById('image')
+    img.src = 'topografik.png'
+}
+
+function changeTemprature() {
+    var img =document.getElementById('image')
+    img.src = 'sıcaklık harita.png'
+}
